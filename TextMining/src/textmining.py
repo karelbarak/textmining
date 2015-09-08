32f5decfef3d663dc817c0ba838f311966651b35
Karel Barak <info@karelbarak.cz>
2015-09-08 14:22:22 +0200

    Change encoding

--- TextMining/src/textmining.py
+++ TextMining/src/textmining.py
@@ -2,7 +2,7 @@
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
 
-__author__ = "Karel Barák"
+__author__ = "Karel Barak"
 __date__ = "$8.9.2015 12:13:46$"
 
 import json
